d767012a1522784cbd2f04ce3270b42cffcddf99
saud-frontend <saudtbc@gmail.com>
2025-02-01 07:51:28 +0500

    Created the project with entityframwork

---
+++ TestDotNetMVC/TestDotNetMVC/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestDotNetMVC.Data;
+using TestDotNetMVC.Models;
+
+namespace TestDotNetMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index() => View(await _repo.GetAllAsync());
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid) { await _repo.AddAsync(product); return RedirectToAction("Index"); }
+            return View(product);
+        }
+
+        public async Task<IActionResult> Edit(int id) => View(await _repo.GetByIdAsync(id));
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            if (ModelState.IsValid) { await _repo.UpdateAsync(product); return RedirectToAction("Index"); }
+            return View(product);
+        }
+
+        public async Task<IActionResult> Delete(int id) => View(await _repo.GetByIdAsync(id));
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _repo.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TestDotNetMVC/TestDotNetMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestDotNetMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
